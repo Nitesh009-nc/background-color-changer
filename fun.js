72058f58c3a901bdd42acbd82540dc4be97bcb8d
Nitesh <85047762+Nitesh009-nc@users.noreply.github.com>
2024-02-02 20:24:28 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,19 @@
+function red(){
+    document.body.style.backgroundColor="#F77B7B";
+}
+function blue(){
+    document.body.style.backgroundColor="lightblue";
+}
+function green(){
+    document.body.style.backgroundColor="#8BE78B";
+}
+function yellow(){
+    document.body.style.backgroundColor="lightyellow";
+}
+function black(){
+    document.body.style.backgroundColor="black";
+}
+function gray(){
+    document.body.style.backgroundColor="lightgray";
+}
+
